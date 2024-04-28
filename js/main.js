5654a089860a5573ab68a4b2279ff17e4e1aaf65
Alister A. Fenix <tech@vpdatacommons.org>
2024-04-28 14:14:48 -0700

    Add logic for loading, filtering and styling historical wildfire geojson

--- js/main.js
+++ js/main.js
@@ -796,99 +796,261 @@ console.log('in the window.geoJsonLayer, removing layer... ', window.geoJsonLaye
     }
 };
 
-// Fetch and display the GeoJSON data on the map and apply custom proportional symbol
-// styling to point features based on the specified column names
-const addNeighborhoodPoints = () => {
-    // Fetch GeoJSON data from the specified local directory
-    fetch("data/crime15_23.geojson")
-        .then(response => response.json())
-        .then(json => {
-            // Store the GeoJSON data in a global variable
-            jsonData = json;
-
-            // Get full year count
-            vandalismCountsByYear = sumYearCounts(json);
-
-            // Initialize the display with the count for the first year in the slider
-            updateTotalVandalismCountDisplay('2015');
-
-            // Create a temporary Leaflet GeoJSON layer to calculate the geographical bounds of the data
-            let vandalLayer = L.geoJSON(json);
-
-            // Adjust the map view to fit the geographical bounds of the GeoJSON data
-            map.fitBounds(vandalLayer.getBounds());
-
-            // Process the GeoJSON data to extract column names
-            let columnNames = getColumnNames(json);
-
-            // Create sequence controls (e.g., sliders, buttons) based on the extracted column names
-            createSequenceControls(columnNames);
-
-            // Ensure the slider displays the year 2015 upon page load.
-            initializeSlider(columnNames);
-
-            // Calculate the minimum and maximum values in the data
-            // and assign them to minValue and maxValue variables using destructuring
-            ({ minValue, maxValue } = calcYearMinMax(json, '2015'));
-
-            // Define class ranges based on geojson data's min/max values
-            classRanges = calculateClassRanges(minValue, maxValue);
-
-            // Create proportional symbols based on the GeoJSON data and the extracted column names
-            createPropSymbols(json, columnNames);
-
-            // Create a legend for the map based on the minimum and maximum values
-            createLegend(minValue, maxValue);
-
-        })
-        // Log an error message if there was an error loading the GeoJSON data
-        .catch(error => console.error('Error loading GeoJSON data:', error));
+// Extract All Unique Years from the MBTS GeoJSON
+const extractUniqueYears = (features) => {
+    const years = new Set();
+    features.forEach(feature => {
+        const igDate = feature.properties.Ig_Date;
+        if (igDate) {
+            const year = igDate.substring(0, 4);
+            years.add(year);
+        }
+    });
+    return Array.from(years).sort();
 };
 
-// The addNeighborhoodBoundaries() function is responsible for fetching and displaying the neighborhood data for the map
-const addNeighborhoodBoundaries = () => {
-    return new Promise((resolve, reject) => {
-        fetch("data/pdx_hoods4326.geojson")
-            .then(response => response.json())
-            .then(data => {
-                let geoJsonLayer = L.geoJSON(data, {
-                    style: {
-                        color: "#ff7800",
-                        weight: 1,
-                        opacity: 1,
-                        fillOpacity: 0.1,
-                        fillColor: "#87CEFA"
-                    },
-                    onEachFeature: (feature, layer) => {
-                        if (feature.properties && feature.properties.NAME) {
-                            layer.bindPopup(feature.properties.NAME);
-                        }
-                    }
-                }).addTo(map);
+const loadFireData  = async () => {
+    try {
+        const response = await fetch(geoJsonPaths["mtbs-fires-pts"]);
+        const data = await response.json();
+        const uniqueYears = extractUniqueYears(data.features);
+        let [minValue, maxValue] = determineMinMax(data.features); // Calculate minimum and maximum burned acres
+        // Initialize the slider with these years
+        setupSliderAndButtons(uniqueYears);
+        filterMapByYear(uniqueYears[0]); // Add the first year's data to the map on page load)
+    } catch (error) {
+        console.error("Failed to load or process geojson:", error);
+    }
+};
 
-                // Now, set up the search feature
-                map.addControl(new L.Control.Search({
-                    position: 'topleft',
-                    layer: geoJsonLayer,
-                    propertyName: 'MAPLABEL',
-                    initial: false,
-                    zoom: 12,
-                    marker: false,
-                    moveToLocation: function (latlng, title, map) {
-                        map.fitBounds(latlng.layer.getBounds());
-                        latlng.layer.fire('click'); // Open the popup
-                    },
-                    filter: function (text, layer) {
-                        // Convert both search text and layer property to lowercase for case-insensitive comparison
-                        return layer.feature.properties.NAME.toLowerCase().indexOf(text.toLowerCase()) !== -1;
-                    }
-                }))
-            })
-            .then(function () {
-                addNeighborhoodPoints();
-                resolve(); // Resolve the promise after adding the layer
-            })
-            .catch(error => console.error('Error loading GeoJSON data:', error));
+// Function to add fire data to the map
+const addFireDataToMap = (geojsonData) => {
+    if (window.geoJsonLayer) {
+        map.removeLayer(window.geoJsonLayer); // Remove existing layer if it exists
+    }
+    let [minValue, maxValue] = determineMinMax(geojsonData.features);
+
+    // create the legend using the min and max values from the filtered year data
+    createProportionalLegend(minValue, maxValue);
+
+    window.geoJsonLayer = L.geoJSON(geojsonData, {
+        pointToLayer: function (feature, latlng) {
+            // Determine fill color based on fire incident type
+            let fillColor;
+            switch (feature.properties.Incid_Type) {
+                case 'Wildfire':
+                    fillColor = '#E85D04'; // Red Orange for Wildfire
+                    break;
+                case 'Prescribed Fire':
+                    fillColor = '#FF9A8C'; // Soft Coral for Prescribed Fire
+                    break;
+                case 'Unknown':
+                        fillColor = '#787878'; // Gray for Unknown
+                        break;
+                case 'Wildland Fire Use':
+                        fillColor = '#82c785'; // Light Green for Beneficial Wildfire/Managed Wildfire
+                        break;
+                default:
+                    fillColor = '#ff7800'; // Gray if type is Unknown
+            }
+            return L.circleMarker(latlng, {
+                radius: calcPropRadius(minValue, feature.properties.BurnBndAc),
+                fillColor: fillColor,
+                color: "#fff",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            });
+        },
+        onEachFeature: (feature, layer) => {
+            if (feature.properties) {
+                // Convert string to Date object
+                const dateString = feature.properties.Ig_Date;
+                const date = new Date(dateString);
+                const formattedDate = date.toLocaleDateString("en-US");  // Output: "7/14/1984"
+                const popupContent = `<h3>Fire Name: ${feature.properties.Incid_Name || 'No Name'}</h3>
+                                      <p>Ignition Date: ${formattedDate}</p>
+                                      <p>Acres Burned: ${feature.properties.BurnBndAc?.toLocaleString()}</p>
+                                      <p>Type of Fire: ${feature.properties.Incid_Type}</p>`;
+                layer.bindPopup(popupContent);
+            }
+        }
+    }).addTo(map);
+};
+
+// Function to calculate the radius of the circle marker
+const calcPropRadius = (minValue, burnedAcres) => {
+    // Define a minimum radius for the symbol, to ensure it's always visible
+    const minRadius = 1;
+    // Calculate the radius of the symbol using the Flannery Appearance Compensation formula
+    // This formula is used to adjust the size of the symbol proportionally based on the attribute value
+    const radius = 1.0083 * Math.pow(burnedAcres / minValue, 0.5715) * minRadius;
+    return radius;
+};
+
+const setupSliderAndButtons = (years) => {
+    const slider = document.getElementById('yearSlider');
+    const rangeValueDisplay = document.getElementById('rangeValue');
+    const reverseButton = document.getElementById('reverse');
+    const forwardButton = document.getElementById('forward');
+
+    // Set the slider properties based on the years array
+    slider.min = 0;  // Start index at 0 for simplicity in indexing
+    slider.max = years.length - 1;  // Max index is the last index in the years array
+    slider.value = 0;  // Default to the first year
+    rangeValueDisplay.textContent = years[0];
+
+    // Function to update the slider appearance and value
+    function updateSliderAppearance(index) {
+        const percentage = (index / (years.length - 1)) * 100;
+        slider.style.background = `linear-gradient(to right, red ${percentage}%, grey ${percentage}%)`;
+        rangeValueDisplay.textContent = years[index];
+        filterMapByYear(years[index]);
+    }
+
+    // Initial map load with the first year
+    updateSliderAppearance(0);
+
+    // Event listener for manual slider adjustments
+    slider.oninput = () => {
+        updateSliderAppearance(parseInt(slider.value, 10));
+    };
+
+    // Setup button functionalities to adjust slider
+    reverseButton.addEventListener('click', function() {
+        if (parseInt(slider.value, 10) > 0) {
+            slider.value = parseInt(slider.value, 10) - 1;
+            updateSliderAppearance(slider.value);
+        }
+    });
+
+    forwardButton.addEventListener('click', function() {
+        if (parseInt(slider.value, 10) < years.length - 1) {
+            slider.value = parseInt(slider.value, 10) + 1;
+            updateSliderAppearance(slider.value);
+        }
+    });
+};
+
+const filterMapByYear = (year) => {
+    fetch(geoJsonPaths["mtbs-fires-pts"])
+        .then(response => response.json())
+        .then(data => {
+            const filteredData = {
+                type: 'FeatureCollection',
+                features: data.features.filter(feature => feature.properties.Ig_Date?.substring(0, 4) === year)
+            };
+            addFireDataToMap(filteredData);
+        })
+        .catch(error => {
+            console.error("Error filtering data:", error);
+        });
+};
+
+const calculateClassRanges = (minValue, maxValue) => {
+    const range = maxValue - minValue;
+    const classWidth = range / 3;  // Creating three classes
+    // Return an array of objects, each defining a class range and a label
+    return [
+        { min: minValue, max: minValue + classWidth, label: `${minValue.toLocaleString()} - ${Math.round(minValue + classWidth).toLocaleString()} acres` },
+        { min: minValue + classWidth, max: minValue + 2 * classWidth, label: `${Math.round(minValue + classWidth).toLocaleString()} - ${Math.round(minValue + 2 * classWidth).toLocaleString()} acres`},
+        { min: minValue + 2 * classWidth, max: maxValue, label: `${Math.round(minValue + 2 * classWidth).toLocaleString()} - ${maxValue.toLocaleString()} acres` },
+    ];
+};
+
+/**
+ * Function to calculate the minimum and maximum values of a given attribute in a set of GeoJSON features.
+ * @param {Array} features - An array of GeoJSON features.
+ * @param {string} attribute - The attribute to calculate the minimum and maximum values for.
+ * @returns {Array} An array containing the minimum and maximum values of the specified attribute.
+ */
+const determineMinMax = (features) => {
+    // Initialize minimum and maximum values to infinity and negative infinity respectively
+    let min = Infinity, max = -Infinity;
+    // Iterate over each feature in the array of GeoJSON features and update the minimum and maximum values accordingly
+    features.forEach(feature => {
+        // Get the value of the specified attribute for the current feature
+        const acres = feature.properties.BurnBndAc;
+        // Update the minimum value if the current value is smaller
+        min = Math.min(min, acres);
+        // Update the maximum value if the current value is larger
+        max = Math.max(max, acres);
+    });
+    // Return an array containing the minimum and maximum values of the specified attribute
+    return [min, max];
+};
+
+/**
+ * Function to create a dynamic map legend
+ * @param {number} minValue - The minimum value for the legend
+ * @param {number} maxValue - The maximum value for the legend
+ * @returns {void} - This function does not return any value, it creates a dynamic map legend
+ */
+function createProportionalLegend(minValue, maxValue) {
+    const legendContainer = document.getElementById('proportional-container');
+    legendContainer.innerHTML = '';  // Clear existing content
+    // Calculate the class ranges based on the minimum and maximum values
+    const range = maxValue - minValue;
+    const classWidth = range / 3;  // Creating three classes
+    const classes = [
+        { min: minValue, max: minValue + classWidth, label: `${minValue.toLocaleString()} - ${Math.round(minValue + classWidth).toLocaleString()} acres` },
+        { min: minValue + classWidth, max: minValue + 2 * classWidth, label: `${Math.round(minValue + classWidth).toLocaleString()} - ${Math.round(minValue + 2 * classWidth).toLocaleString()} acres`},
+        { min: minValue + 2 * classWidth, max: maxValue, label: `${Math.round(minValue + 2 * classWidth).toLocaleString()} - ${maxValue.toLocaleString()} acres` },
+    ];
+
+    // Reverse the order of the classes array so the legend will be displayed in the correct order
+    classes.reverse();
+
+    // Create the legend-item container
+    classes.forEach(cls => {
+        const radius = calcPropRadius(minValue, (cls.max + cls.min) / 2);
+        // Create the legend-item container
+        const itemContainer = document.createElement("div");
+        itemContainer.className = "legend-item-proportional";
+
+        // Create the circle and value elements
+        const legendCircle = document.createElement('div');
+        legendCircle.className = 'legendCircle-proportional';
+        legendCircle.style.width = legendCircle.style.height = `${radius * 2}px`;
+        let legendValue = document.createElement('div');
+        legendValue.className = 'legendValue-proportional';
+        legendValue.textContent = cls.label;
+        //legendCircle.append(legendValue);
+
+        // Append elements in the correct hierarchy
+        itemContainer.appendChild(legendCircle);
+        itemContainer.appendChild(legendValue);
+        legendContainer.appendChild(itemContainer);
+    });
+}
+
+const createCloroplethLegend = () => {
+    const legendContainer = document.getElementById('cloropleth-container');
+    legendContainer.innerHTML = ''; // Clear existing content
+
+    const classes = [
+        { label: 'Wildfire', color: '#E85D04' },
+        { label: 'Prescribed Fire', color: 'yellow' },
+        { label: 'Wildland Fire Use', color: '#82c785' },
+        { label: 'Unknown', color: '#787878' },
+    ];
+
+    classes.forEach(cls => {
+        const itemContainer = document.createElement("div");
+        itemContainer.className = "legend-item-cloropleth";
+
+        const legendCircle = document.createElement('div');
+        legendCircle.className = 'legendCircle-cloropleth';
+        legendCircle.style.width = legendCircle.style.height = '30px';
+        legendCircle.style.backgroundColor = cls.color;
+
+        const legendValue = document.createElement('div');
+        legendValue.className = 'legendValue-cloropleth';
+        legendValue.textContent = cls.label;
+
+        itemContainer.appendChild(legendCircle);
+        itemContainer.appendChild(legendValue);
+        legendContainer.appendChild(itemContainer);
     });
 };
 
